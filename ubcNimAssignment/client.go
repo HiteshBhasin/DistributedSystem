0e95dcfac230718cc225c997ca471a9e6ea61e12
hitesh bhasin <bhasinsukh@gmail.com>
2026-07-21 18:48:29 -0500

    new commit

--- ubcNimAssignment/client.go
+++ ubcNimAssignment/client.go
@@ -1,13 +1,24 @@
 package client
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"ubcNimAssignment/blueprint"
 )
 
+type Config struct {
+	ServerAddress string `json:"server UDP ADD"`
+}
+
+var config = Config{
+	ServerAddress: "197.0.0.8080",
+}
+
 func main() {
-	msg = blueprint.StateMoveMessage
+	msg := blueprint.StateMoveMessage{}
+	byteData = ReadConfig("config.json")
+
 }
 
 func ReadConfig(configFile string) []byte {
@@ -17,4 +28,11 @@ func ReadConfig(configFile string) []byte {
 
 	}
 	return fileBytes
+}
+
+func WriteByteFile(data []byte, msg blueprint.StateMoveMessage) {
+	err := json.Unmarshal(data, &msg)
+	if err != nil {
+		fmt.Printf("successfully generated the file and returned the %v\n", err)
+	}
 }
